65d17398fa5c0f778e56add14e5a7ae8d4417dfc
Subhashx17 <k.saisubhash.601@gmail.com>
2026-08-12 23:52:50 +0530

    add string.c file

---
+++ strings.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(){
+    char name[50];
+    printf("enter your first name : ");
+    scanf("%s",name);
+    printf("Your First name is : %s", name);
+    return 0;
+}
+
