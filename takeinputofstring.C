081fd545d450274506dc669247792426b0c87770
Subhashx17 <k.saisubhash.601@gmail.com>
2026-08-13 16:51:01 +0530

    added takinputofstring.C file

---
+++ takeinputofstring.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    char name[100];
+    char ch;
+    int i=0;
+
+    while( i != '\n'){
+        scanf("%c",&ch);
+        name[i]=ch;
+        i++;
+    }
+    name[i]='\0';
+    puts(name);
+    return 0;
+}
