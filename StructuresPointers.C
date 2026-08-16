88c5b5b9fb6fb4833056d10a3c34f8c43eaf3dc4
Subhashx17 <k.saisubhash.601@gmail.com>
2026-08-16 12:03:09 +0530

    added StructurePointer files

---
+++ StructuresPointers.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+struct student {
+    int rollno;
+    float cgpa;
+    char name[100];
+   };
+
+int main(){
+    struct student s1 = {7093 , 1.4 , "Subahsh"};
+    printf("student roll no : %d \n ",s1.rollno);
+    printf("student cgpa : %f \n ",s1.cgpa);
+    printf("student Name : %s \n ",s1.name);
+   
+
+    struct student *ptr = &s1;
+    printf("student roll no : %d \n ", (*ptr).rollno);
+    return 0;
+}
