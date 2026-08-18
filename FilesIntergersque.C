a91ca24f890b99d5150221d7d51a03fe72e9e39b
Subhashx17 <k.saisubhash.601@gmail.com>
2026-08-18 14:47:31 +0530

    added Integersque.txt and FilesIntegersque.C files

---
+++ FilesIntergersque.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    FILE *fhie;
+    fhie = fopen("integersque.txt","w");
+
+    int a;
+    fscanf(fhie, "%d", &a);
+    printf("%d\n",a);
+    fscanf(fhie, "%d", &a);
+    printf("%d\n",a);
+    fscanf(fhie, "%d", &a);
+    printf("%d\n",a);
+    fscanf(fhie, "%d", &a);
+    printf("%d\n",a);
+    fscanf(fhie, "%d", &a);
+    printf("%d\n",a);
+    
+    
+
+    fclose(fhie);
+    return 0;
+}
