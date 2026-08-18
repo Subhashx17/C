67545154d33dac5ebdffa29cb2d4f2762e08a844
Subhashx17 <k.saisubhash.601@gmail.com>
2026-08-18 14:22:44 +0530

    updated FilesIntro.C , and added test.txt file

--- FilesIntro.C
+++ FilesIntro.C
@@ -6,11 +6,20 @@ int main(){
     // fclose(fptr);
 
     FILE *fptr;
-    fptr = fopen("test.txt","r");
+    fptr = fopen("test.txt","w");
 
-    char ch[100];
-    fscanf(fptr, "%[^\n]", ch);
-    printf("%s", ch);
+    // char ch[100];
+    // fscanf(fptr, "%[^\n]", ch);
+    // printf("%s", ch);
+    // fclose(fptr);
+
+    char ch[]="Mango ";
+    fprintf(fptr, "%s",ch);
+    fclose(fptr);
+
+    fptr = fopen("test.txt","a");
+    char ch2[]="Thuppas Yedava";
+    fprintf(fptr, "%s",ch2);
     fclose(fptr);
     return 0;
 }
